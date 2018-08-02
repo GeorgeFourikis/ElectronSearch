fa221757beb409faa5146a10198eaee3aadfbbc8
Gio <georgefourikis@hotmail.gr>
2018-08-02 10:30:35 +0300

    minor quotation fixes

--- public/inject.js
+++ public/inject.js
@@ -1,14 +1,14 @@
 const { ipcRenderer } = require('electron')
 
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
     //setting the flag so it will be triggered once in a session
     let flag = false
-    let existingValue = ""
-    let myInput = document.querySelector("#lst-ib")
+    let existingValue = ''
+    let myInput = document.querySelector('#lst-ib')
 
     myInput.addEventListener("keypress", () => {
         existingValue = myInput.value
-        if (existingValue.toLowerCase().includes("digital attitude") && flag === false) {
+        if (existingValue.toLowerCase().includes('digital attitude') && flag === false) {
             ipcRenderer.sendSync('synchronous-message', 'Searched') // sends this back to main renderer notifier object 
 
             //set the flag to true, means that it wont give a new Notification again
